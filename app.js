7163d6369396937d466c3ce85b12eacd4db97b0c
Fariz27 <fariz313.akbar@gmail.com>
2020-10-15 22:48:25 +0700

    commit lat 2

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var app = new Vue({
-    
+    el  :   '#app',
+    data:{
+        'message':'Hello World'
+    },
+    methods:{
+            greet: function(name){
+            return "Good Morning, " + name;
+        }
+    }
 })
