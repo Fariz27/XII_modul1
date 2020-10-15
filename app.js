1e7feb066058e7ce404127c3f6d6a8293cc8fb8a
Fariz27 <fariz313.akbar@gmail.com>
2020-10-15 23:10:47 +0700

    commit lat6

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var app = new Vue({
         'message'       :'Hello World',
         'url_telkom'    :'https://smktelkom-mlg.sch.id',
         'nama'          :'',
-        'stock'         :6
+        'stock'         :6,
+        'kelas'         :['XIIR1','XIIR2','XIIR3','XIIR4','XIIR5','XIIR6','XIIR7']
     },
     methods:{
             greet: function(name){
